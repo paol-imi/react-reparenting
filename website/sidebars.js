bf8c6c1157e71785f754644625404c8f677295c7
Paol-imi <paololongo.git@gmail.com>
2020-05-11 12:12:43 +0200

    Added Documentation 📖

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   docs: {
     'Getting Started': ['introduction', 'installation', 'usage'],
     Guides: ['reparentable', 'parent', 'createparent', 'useparent', 'renderer'],
-    'API Reference': ['tools', 'core'],
+    'API Reference': ['tools'],
   },
 };
